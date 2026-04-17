d82dc58123f3bce0b889af0cea7778aced3711cb
Owen Johnson <12706085+owenjohnson@users.noreply.github.com>
2026-04-17 16:58:06 -0400

    chore: update formula to v0.7.11

--- Formula/maintainer.rb
+++ Formula/maintainer.rb
@@ -1,14 +1,14 @@
 class Maintainer < Formula
   desc "Automated PR review pipeline for centient-labs"
   homepage "https://github.com/centient-labs/maintainer"
-  version "0.7.10"
+  version "0.7.11"
 
   depends_on :macos
   depends_on arch: :arm64
   depends_on "centient-labs/centient/engram" => :recommended
 
   url "https://github.com/centient-labs/homebrew-centient/releases/download/maintainer-v#{version}/maintainer-macos-arm64.tar.gz"
-  sha256 "0fe8a6f14b3cde263bf2a4016e587ca785cf29940569ae03596b1440680c79c3"
+  sha256 "5c12130ae1881e83951c84f938a2827a4fc98657b87c54651f7918afe673f8a4"
 
   def install
     bin.install "maintainer"
